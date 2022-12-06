8026b9817cff3d56a75c0f0b1ed9ce436553477d
alcl2000 <amy.555.lewis@gmail.com>
2022-12-06 17:06:17 +0000

    display error func

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,22 @@ var resultModal = new bootstrap.Modal(document.getElementById('resultsModal'))
 document.getElementById('status').addEventListener('click', e => getStatus(e));
 document.getElementById('submit').addEventListener('click', e => postForm(e));
 
+function processOptions(form){
+    
+    let optArray = [];
+    for(let entry of form.entries()){
+        if(entry[0] === 'options'){
+            optArray.push(entry[1])
+        }
+    }
+    form.delete('options');
+    form.append('options', optArray.join());
+
+    return form;
+}
+
 async function postForm(e){
-    const form = new FormData(document.getElementById('checksform'));
+    const form = processOptions(new FormData(document.getElementById('checksform')));
     
     const response = await fetch(API_URL, {
         method: "POST",
@@ -36,9 +50,9 @@ function displayErrors(data){
     else{
         results = `<div>Total Errors: <span class='error_count'>${data.total_errors}</span></div>`
         for(let error of data.error_list){
-            results += `<div>At Line <span class = 'line'><${error.line}/span>,`;
-            results += `& column <span class='line'>${error.column}</span></div>`;
-            results += `<div class='error'>${error}</div>`
+            results += `<div>At Line <span class = 'line'>${error.line}</span>,`;
+            results += `& column <span class='line'>${error.col}</span></div>`;
+            results += `<div class='error'>${error.error}</div>`
         }
     }
     document.getElementById('results-content').innerHTML = results;
